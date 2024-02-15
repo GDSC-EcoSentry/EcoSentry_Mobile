499fa9543e580c4dda4a413b7e63832728cbefc2
AnonyFriday <duykimvu75@gmail.com>
2024-02-16 06:21:07 +0700

    Handle pushBack button

--- app/src/main/java/com/observers/ecosentry_mobile/DrawerActivity.java
+++ app/src/main/java/com/observers/ecosentry_mobile/DrawerActivity.java
@@ -1,20 +1,26 @@
 package com.observers.ecosentry_mobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DrawerActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // ================================
     // == Fields
     // ================================
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolbar;
+    private NavigationView mNavigationView;
 
     // ================================
     // == Life Cycle
@@ -52,5 +58,38 @@ public class DrawerActivity extends AppCompatActivity {
                 mDrawerLayout, mToolbar, R.string.drawer_nav_open, R.string.drawer_nav_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    private void setUpNavigationView() {
+        mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_dashboard) {
+
+        } else if (id == R.id.nav_profile) {
+
+        }
+
+        // Close drawer if did not choose anything
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // If Drawer is open, push back and close it
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            // If drawer is close, just push back as usual
+            super.onBackPressed();
+        }
     }
 }

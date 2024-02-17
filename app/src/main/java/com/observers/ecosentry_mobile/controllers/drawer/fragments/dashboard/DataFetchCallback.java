5123c4eadd968f4c8bec60feae2402d7ec8db9d8
meewaldor <tieumylam91@gmail.com>
2024-02-17 13:22:47 +0700

    Get stations from firestore

--- app/src/main/java/com/observers/ecosentry_mobile/controllers/drawer/fragments/dashboard/DataFetchCallback.java
+++ app/src/main/java/com/observers/ecosentry_mobile/controllers/drawer/fragments/dashboard/DataFetchCallback.java
@@ -1,10 +1,13 @@
 package com.observers.ecosentry_mobile.controllers.drawer.fragments.dashboard;
 
 import com.observers.ecosentry_mobile.models.node.Node;
+import com.observers.ecosentry_mobile.models.station.Station;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 @FunctionalInterface
 public interface DataFetchCallback {
-    void onDataFetched(ArrayList<Node> nodes);
+    void onDataFetched(TreeMap nodes);
 }

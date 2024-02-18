9aed33bfdcf1c958cbb08b336f4e1a4ebe9fa689
lamee91 <myltse180542@fpt.edu.vn>
2024-02-18 09:59:09 +0700

    Check Login by using Firebase Auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
     // Circle Image View
     implementation("de.hdodenhof:circleimageview:3.1.0")
--- app/src/main/java/com/observers/ecosentry_mobile/controllers/authentication/LoginActivity.java
+++ app/src/main/java/com/observers/ecosentry_mobile/controllers/authentication/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.observers.ecosentry_mobile.controllers.authentication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -14,10 +16,27 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.observers.ecosentry_mobile.R;
 import com.observers.ecosentry_mobile.controllers.drawer.DrawerActivity;
 import com.observers.ecosentry_mobile.models.user.User;
@@ -38,6 +57,10 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText mTextInputEditTextEmailLogin, mTextInputEditTextPasswordLogin;
     private MaterialButton mButtonNormalLogin;
     private MaterialTextView mTextViewRegisterNowLogin;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 20;
 
     // ======================
     // == Life Cycle
@@ -60,8 +83,35 @@ public class LoginActivity extends AppCompatActivity {
         // Setup listeners
         mButtonNormalLogin.setOnClickListener(setUpButtonNormalLogin());
         mButtonGoogleLogin.setOnClickListener(setUpButtonGoogleLogin());
+
+        // Setup Firebase Firestore
+        db = FirebaseFirestore.getInstance();
+
+        // Setup FirebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Setup the Google sign-in proccess
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+
+            } catch (Exception e) {
+                Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     // ======================
     // == Methods
     // ======================
@@ -95,17 +145,35 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
                     // If success, do the firebase authentication here
-
+                    firebaseAuth.signInWithEmailAndPassword(email,password)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                                    String email = firebaseUser.getEmail();
+                                    User user = new User();
+                                    user.setEmail(email);
+                                    Map<String, Object> data = new HashMap();
+                                    data.put("user", user);
+                                    ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(LoginActivity.this, "Incorrect email or password", Toast.LENGTH_LONG).show();
+                                }
+                            });
 
                     // After login sucesss, get user object
                     // then passing to intent,
                     // then move to Drawer Activity
 
                     // FIXME: Fake User, return the user, add to the HashMap, pass to function
-                    User user = new User();
-                    Map<String, Object> data = new HashMap();
-                    data.put("user", user);
-                    ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
+//                    User user = new User();
+//                    Map<String, Object> data = new HashMap();
+//                    data.put("user", user);
+//                    ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
                 } else {
                     // If fail, showing toast
                     Toast.makeText(LoginActivity.this, "Please input email and password", Toast.LENGTH_LONG).show();
@@ -125,17 +193,49 @@ public class LoginActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Put your google authentication code in here
+                // Retrieve sign-in intent and start sign-in activity
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent,RC_SIGN_IN);
 
 
                 // FIXME: Fake User, return the user, add to the HashMap, pass to function
-                User user = new User();
-                user.setEmail("abc@gmail.com");
-                user.setUsername("Cái này tui test thôi nha");
-                Map<String, Object> data = new HashMap();
-                data.put("user", user);
-                ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
+//                User user = new User();
+//                user.setEmail("abc@gmail.com");
+//                user.setUsername("Cái này tui test thôi nha");
+//                Map<String, Object> data = new HashMap();
+//                data.put("user", user);
+//                ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
             }
         };
+    }
+    /**
+    Authenticates the user with Firebase using the provided ID token.
+     */
+    public void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            String email = firebaseUser.getEmail();
+                            String userID = firebaseUser.getUid();
+                            String userName = firebaseUser.getDisplayName();
+                            String photoUrl = firebaseUser.getPhotoUrl().toString();
+                            User user = new User(email,userID,userName,photoUrl,"user");
+                            Map<String, Object> data = new HashMap();
+                            data.put("user",user);
+                            // Add user to firestore
+                            DocumentReference userRef = db.collection("users").document(userID);
+                            userRef.get().addOnSuccessListener(command -> {
+                                if (!command.exists()) {
+                                    userRef.set(user);
+                                }
+                            });
+                            ActivityHelper.moveToNextActivity(LoginActivity.this, DrawerActivity.class, data);
+                        }
+                    }
+                });
     }
 }
